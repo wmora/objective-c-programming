31d872574c0353161fb3155992cfe84402958afb
William Mora <wilmor24@gmail.com>
2013-12-10 22:16:49 -0430

    Chapter 2

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by William Mora on 10/12/13.
+//  Copyright (c) 2013 wmora. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    // Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /* Is that actually any good?
+        Maybe it needs a rewrite. */
+
+    return 0;
+}
+
